aaf17e86dba75e8b92bd47be411f964e6065a148
Matin.N.D.S <matin.n.d.s@gmail.com>
2022-09-21 13:41:46 +0430

    Add animation changer

--- Assets/Scripts/Logic/AnimationChanger.cs
+++ Assets/Scripts/Logic/AnimationChanger.cs
@@ -1,6 +1,20 @@
+using UnityEngine;
+
 namespace Logic
 {
     public class AnimationChanger
     {
+        private static readonly int Start = Animator.StringToHash("Start");
+        private static readonly int Setting = Animator.StringToHash("Setting");
+
+        void StartAnimationChange(Animator animator)
+        {
+            animator.SetBool(Start, true);
+        }
+
+        void SettingAnimationChange(Animator animator)
+        {
+            animator.SetBool(Setting, true);
+        }
     }
 }
