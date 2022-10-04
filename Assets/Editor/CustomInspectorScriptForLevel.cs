37e0cc4ffbcfb9ec17c5192189646aabc15589e5
Matin.N.D.S <matin.n.d.s@gmail.com>
2022-10-04 11:11:59 +0330

    Finally 2D Array Serialization

--- Assets/Editor/CustomInspectorScriptForLevel.cs
+++ Assets/Editor/CustomInspectorScriptForLevel.cs
@@ -1,40 +1,44 @@
-// using System;
-// using Model;
-// using UnityEditor;
-// using UnityEngine;
-//
-// namespace Editor
-// {
-//     [CustomEditor(typeof(Level))]
-//     public class CustomInspectorScriptForLevel : UnityEditor.Editor
-//     {
-//         Level _targetScript;
-//
-//         void OnEnable()
-//         {
-//             _targetScript = target as Level;
-//         }
-//         public override void OnInspectorGUI()
-//         {
-//             base.OnInspectorGUI();
-//             Level.BlockRows = EditorGUILayout.IntField(Level.BlockRows);
-//             Level.BlockColumns = EditorGUILayout.IntField(Level.BlockColumns);
-//
-//             EditorGUILayout.BeginHorizontal();
-//             for (int y = 0; y < Level.BlockColumns; y++)
-//             {
-//                 EditorGUILayout.BeginVertical();
-//                 for (int x = 0; x < Level.BlockRows; x++)
-//                 {
-//                     _targetScript.Blocks[x, y] =
-//                         (GameObject)EditorGUILayout.ObjectField(_targetScript.Blocks[x, y], typeof(GameObject), false);
-//                     
-//                 }
-//
-//                 EditorGUILayout.EndVertical();
-//             }
-//
-//             EditorGUILayout.EndHorizontal();
-//         }
-//     }
-// }
+using System;
+using Model;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor
+{
+    [CustomEditor(typeof(Level))]
+    public class CustomInspectorScriptForLevel : UnityEditor.Editor
+    {
+        Level _targetScript;
+
+        void OnEnable()
+        {
+            _targetScript = target as Level;
+        }
+        public override void OnInspectorGUI()
+        {
+            // base.OnInspectorGUI();
+            
+            
+            Level.BlockRows = EditorGUILayout.IntField(Level.BlockRows);
+            Level.BlockColumns = EditorGUILayout.IntField(Level.BlockColumns);
+
+            EditorGUILayout.BeginHorizontal();
+            for (int y = 0; y < Level.BlockColumns; y++)
+            {
+                EditorGUILayout.BeginVertical();
+                for (int x = 0; x < Level.BlockRows; x++)
+                {
+                    _targetScript.Blocks[x, y] =
+                        (GameObject)EditorGUILayout.ObjectField(_targetScript.Blocks[x, y],
+                            typeof(GameObject), 
+                            false);
+                    
+                }
+
+                EditorGUILayout.EndVertical();
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+}
--- Assets/Scripts/Model/Level.cs
+++ Assets/Scripts/Model/Level.cs
@@ -1,44 +1,51 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Model
 {
     [CreateAssetMenu(fileName = "Level", menuName = "Level", order = 2)]
-    public class Level : ScriptableObject , ISerializationCallbackReceiver
+    public class Level : ScriptableObject, ISerializationCallbackReceiver
     {
         public static int BlockRows = 4;
         public static int BlockColumns = 8;
-        public GameObject[,] Blocks = new GameObject[BlockRows,BlockColumns];
-        
-        
-        [SerializeField] private List<BlockObjectRows> _tempBlocks;
-        public class BlockObjectRows
+        public GameObject[,] Blocks = new GameObject[BlockRows, BlockColumns];
+
+
+        [SerializeField, HideInInspector] private List<BlockObjectRows> _tempBlocks;
+
+        [Serializable]
+        private class BlockObjectRows
         {
             public List<GameObject> Blocks = new List<GameObject>();
         }
+
         public void OnBeforeSerialize()
         {
-            //convert Blocks to _tempBlocks
+            Debug.Log("OnBeforeSerialize Called");
+
+            // convert Blocks to _tempBlocks
             _tempBlocks = new List<BlockObjectRows>();
             for (int i = 0; i < BlockRows; i++)
             {
                 _tempBlocks.Add(new BlockObjectRows());
                 for (int j = 0; j < BlockColumns; j++)
                 {
-                   _tempBlocks[i].Blocks.Add(Blocks[i,j]);
+                    _tempBlocks[i].Blocks.Add(Blocks[i, j]);
                 }
             }
         }
 
         public void OnAfterDeserialize()
         {
+            Debug.Log("OnAfterDeserialize Called");
+
             //convert _tempBlocks to Blocks
-            _tempBlocks = new List<BlockObjectRows>();
             for (int i = 0; i < BlockRows; i++)
             {
                 for (int j = 0; j < BlockColumns; j++)
                 {
-                    Blocks[i,j] = _tempBlocks[j].Blocks[i];
+                    Blocks[i, j] = _tempBlocks[i].Blocks[j];
                 }
             }
         }
