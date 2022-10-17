687566f53a1db6cf9cbf64e7db659cc57197a80f
Matin.N.D.S <matin.n.d.s@gmail.com>
2022-10-17 15:09:56 +0330

    Add base editor keys

--- Assets/Editor/LevelDesign.cs
+++ Assets/Editor/LevelDesign.cs
@@ -6,7 +6,10 @@ namespace Editor
 {
     public class LevelDesign : EditorWindow
     {
+        private int _screenWidth = 1500;
+        private int _screenLength = 1100;
         public LevelManageData _levelManageData;
+
         [MenuItem("LevelDesign/LevelDesign")]
         public static void ShowWindow()
         {
@@ -17,16 +20,36 @@ namespace Editor
         {
             _levelManageData = CreateInstance<LevelManageData>();
             _levelManageData = Resources.Load<LevelManageData>("LevelManageDatas/LevelmanageData");
-            position = new Rect(100,100,500,100);
+            position = new Rect(position.center.x, position.center.y, _screenWidth, _screenLength);
         }
 
         private void OnGUI()
         {
+            EditorGUILayout.BeginHorizontal();
             EditorGUILayout.BeginVertical();
-            EditorGUILayout.ObjectField(Resources.Load<GameObject>("Prefabs/Blocks/Normal-Block"), typeof(GameObject), false);
-            EditorGUILayout.ObjectField(Resources.Load<GameObject>("Prefabs/Blocks/Bomb-Block"), typeof(GameObject), false);
+            foreach (var gameObject in Resources.LoadAll<GameObject>("Prefabs/Blocks"))
+            {
+                GUILayout.Button(gameObject.GetComponent<SpriteRenderer>().sprite.texture);
+            }
+
             EditorGUILayout.EndVertical();
-            
+            EditorGUILayout.BeginVertical();
+            for (int temp = 0; temp < LevelManageData.LevelNumber; temp++)
+            {
+                GUILayout.Button((temp + 1).ToString());
+            }
+            EditorGUILayout.EndVertical();
+            for (int y = 0; y < Level.BlockColumns; y++)
+            {
+                EditorGUILayout.BeginVertical();
+                for (int x = 0; x < Level.BlockRows; x++)
+                {
+                    GUILayout.Button("Blcok "+((y+1)*(x+1)));
+                }
+
+                EditorGUILayout.EndVertical();
+            }
+            EditorGUILayout.EndHorizontal();
         }
     }
 }
