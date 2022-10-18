572f26d146ba0829c7c1b5b04b3269185e80e708
Matin.N.D.S <matin.n.d.s@gmail.com>
2022-10-18 12:25:24 +0330

    Update LevelDesign.cs

--- Assets/Editor/LevelDesign.cs
+++ Assets/Editor/LevelDesign.cs
@@ -6,8 +6,11 @@ namespace Editor
 {
     public class LevelDesign : EditorWindow
     {
-        private int _screenWidth = 1500;
-        private int _screenLength = 1100;
+        private int _screenWidth = 1100;
+        private int _screenLength = 250;
+        private int _normalButtonWidth = 85;
+        private int _normalButtonHeight = 35;
+        private int _spaceBetweenTables = 20;
         public LevelManageData levelManageData;
         private int _levelNumber;
 
@@ -26,36 +29,56 @@ namespace Editor
 
         private void OnGUI()
         {
+            GUILayout.BeginArea(new Rect(0, 0, 1000, 250));
+
             EditorGUILayout.BeginHorizontal();
 
             EditorGUILayout.BeginVertical();
 
+            GUILayout.Label("Choose your Block");
             ObjectButtonsActions();
 
             EditorGUILayout.EndVertical();
 
+            EditorGUILayout.Space(_spaceBetweenTables);
+            
             EditorGUILayout.BeginVertical();
 
+            GUILayout.Label("Choose your Levels");
             LevelButtunsActions();
 
             EditorGUILayout.EndVertical();
+            
+            EditorGUILayout.Space(_spaceBetweenTables);
+
+            EditorGUILayout.BeginVertical(GUILayout.Height(175));
+
+            GUILayout.Label("Design your Level");
+
+            EditorGUILayout.BeginHorizontal(GUILayout.Width(175));
 
             BlockButtonsActions();
 
             EditorGUILayout.EndHorizontal();
+
+            EditorGUILayout.EndVertical();
+
+            EditorGUILayout.EndHorizontal();
+            GUILayout.EndArea();
         }
 
         private void ObjectButtonsActions()
         {
             foreach (var gameObject in Resources.LoadAll<GameObject>("Prefabs/Blocks"))
             {
-                if (GUILayout.Button(gameObject.GetComponent<SpriteRenderer>().sprite.texture))
+                if (GUILayout.Button(gameObject.GetComponent<SpriteRenderer>().sprite.texture,
+                        GUILayout.Height(_normalButtonHeight), GUILayout.MinWidth(_normalButtonWidth)))
                 {
                     Selection.activeObject = gameObject;
                 }
             }
 
-            if (GUILayout.Button("Null"))
+            if (GUILayout.Button("Null", GUILayout.Height(_normalButtonHeight), GUILayout.MinWidth(_normalButtonWidth)))
             {
                 Selection.activeObject = null;
             }
@@ -65,7 +88,8 @@ namespace Editor
         {
             for (int temp = 0; temp < LevelManageData.LevelNumber; temp++)
             {
-                if (GUILayout.Button("Level " + (temp + 1).ToString()))
+                if (GUILayout.Button("Level " + (temp + 1), GUILayout.Height(_normalButtonHeight),
+                        GUILayout.MinWidth(_normalButtonWidth)))
                 {
                     _levelNumber = temp;
                 }
@@ -79,9 +103,11 @@ namespace Editor
                 EditorGUILayout.BeginVertical();
                 for (int x = 0; x < Level.BlockRows; x++)
                 {
+                    EditorGUILayout.BeginHorizontal();
                     if (levelManageData.levels[_levelNumber].Blocks[x, y] == null)
                     {
-                        if (GUILayout.Button("Empty Block"))
+                        if (GUILayout.Button("Empty Block", GUILayout.Height(_normalButtonHeight),
+                                GUILayout.Width(_normalButtonWidth)))
                         {
                             levelManageData.levels[_levelNumber].Blocks[x, y] = (GameObject)Selection.activeObject;
                         }
@@ -89,11 +115,15 @@ namespace Editor
                     else
                     {
                         if (GUILayout.Button(levelManageData.levels[_levelNumber].Blocks[x, y]
-                                .GetComponent<SpriteRenderer>().sprite.texture))
+                                    .GetComponent<SpriteRenderer>().sprite.texture,
+                                GUILayout.Height(_normalButtonHeight),
+                                GUILayout.Width(_normalButtonWidth)))
                         {
                             levelManageData.levels[_levelNumber].Blocks[x, y] = (GameObject)Selection.activeObject;
                         }
                     }
+
+                    EditorGUILayout.EndHorizontal();
                 }
 
                 EditorGUILayout.EndVertical();
